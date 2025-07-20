836ee4764f3dd06966b2989fe8c8c3a310388947
Muhammad Sabbir Hossain Bin Ahmad <your_email@example.com>
2025-07-20 23:16:56 +0600

    Add weather-based activity selection logic

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,12 @@
+weather = "Sunny"
+
+if weather == "Sunny":
+    activity = "Go for a walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Cloudy":
+    activity = "Build a snowman"
+else:
+    activity = "Do nothing"
+
+print(activity)
