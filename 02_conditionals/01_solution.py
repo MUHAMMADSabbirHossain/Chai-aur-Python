aeb0aafd450f3b8cfd166d2abeee5ecfb89842c4
Muhammad Sabbir Hossain Bin Ahmad <your_email@example.com>
2025-07-20 22:45:10 +0600

    Add age classification logic to solution

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,14 @@
+# age = input("Provide me an age: ")
+
+# age_in_int = int(age)
+
+age = 20
+
+if age <13:
+    print("Childs")
+elif age <20:
+    print("Teenagers")
+elif age < 60:
+    print("Adults")
+else:
+    print("Seniors")
